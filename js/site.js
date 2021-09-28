6bad32a0b3f98bfb010fab08f30d5c5ef41876dc
AdamTalleh <46764037+AdamTalleh@users.noreply.github.com>
2021-09-28 15:33:26 +0200

    Hundred to go app
    need only links to social media and git repo

--- js/site.js
+++ js/site.js
@@ -1,5 +1,69 @@
-//call helloworld 
-function HelloWorld(){
-    alert("hello world!");
-     
+// get the values from the page
+///stats or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+    //parse into integers
+    startValue =parseInt(startValue);
+    endValue =parseInt(endValue);
+
+    if (Number.isInteger(startValue)&& Number.isInteger(endValue))
+    {
+         //we call generateNumbers
+
+    
+        let numbers= generateNumbers(startValue,endValue);
+        
+        //we call displayNumbers
+        displayNumbers(numbers);
+        
+    } else {
+    alert("You must enter integers")
+    }
+
+
+   
+
+
+}
+
+//generate numbers from the start value to the end value
+//logic function
+function generateNumbers(sValue,eValue){
+    let numbers = [];
+    //we want to get all the numbers from start to end
+    
+    for (let index = sValue ; index <= eValue ; index++)
+     {
+         //this will excute a loop until index=eValue 
+        numbers.push(index);
+        
+    }
+    return numbers;
+}
+
+// Display the numbers and mark even numbers Bold
+//display or view functions
+function displayNumbers(numbers){
+    let templateRows ="";
+    for (let index = 0; index < numbers.length; index++) {
+        let className="even";
+        let number = numbers[index];
+        if(number%2 == 0){
+          className="even";
+        }
+        else{
+            className="odd";
+        }
+        //this does render correctly with prism see the source
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+
+        
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
+
 }
